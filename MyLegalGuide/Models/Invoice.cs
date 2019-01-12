ea64f927e1801769929a38f3a5adf08a109f341d
ZIGU <ZIGU@ZIGU-PC>
2019-01-12 08:27:08 +0100

    Invoice Model Added

---
+++ MyLegalGuide/Models/Invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLegalGuide.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public int CaseId { get; set; }
+        public float FeeStructure { get; set; }
+        public decimal TotalAmountDue { get; set; }
+        public decimal UnpaidBalance { get; set; }
+
+        public Case Case { get; set; }
+    }
+}
