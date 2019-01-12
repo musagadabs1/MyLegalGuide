922b7c25c702f497117aa0173f20b380ae2d7546
ZIGU <ZIGU@ZIGU-PC>
2019-01-12 08:05:38 +0100

    Case and Client classes added

---
+++ MyLegalGuide/Models/Case.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLegalGuide.Models
+{
+    public class Case
+    {
+        public int CaseId { get; set; }
+        public int ClientId { get; set; }
+        public int StaffId { get; set; }
+        public string CaseName { get; set; }
+        public int CaseNumber { get; set; }
+        public DateTime Opened { get; set; }
+        public PracticeArea PracticeArea { get; set; }
+        public Stage CaseStage { get; set; }
+        public string Description { get; set; }
+        public DateTime StatuteOfLimitation { get; set; }
+    }
+    public enum PracticeArea
+    {
+        Bankruptcy=1,
+        Business,
+        Civil,
+        Criminal_Defense,
+        Divorce_Separation,
+        Employment,
+        EstatePlanning,
+        Family,
+        Tax,
+        Landlord_Tenant
+    }
+    public enum Stage
+    {
+        Discovery=1,
+        In_Trial,
+        On_Hold
+    }
+}
---
+++ MyLegalGuide/Models/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLegalGuide.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public Group ContactGroup { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+    }
+    public enum Group
+    {
+        Client=1,
+        Co_Counsel,
+        Expert,
+        Judge,
+        Unassigned
+    }
+}
